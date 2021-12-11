02ba83f83ef8d24b0009421658ea341e44b74c75
Gabriel Carino <gabejcarino@gmail.com>
2021-12-10 16:17:08 -0800

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Gabe is the champion";
+document.body.append(newHeader);
